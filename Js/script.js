e3f84f7555393095a3dc6c7a3c59b202d1cb78b2
headlessNode <harriskhan047@outlook.com>
2023-09-28 01:23:41 +0500

    Replace previously written code with a new version - create a main game object. - create Player object within game object, for players creation. - create a gameBoard object with contains an array having the player   choices. - create a display object to display the players' choices on the board.

--- Js/script.js
+++ Js/script.js
@@ -1,31 +1,83 @@
-
 const game = (()=>{
     
-    const gameBoard = ()=>{
-        let board = ['X1','O2','X3','O4','X5','O6','X7','O8','X9'];
-        
-        return board;
-    };
-        
+    const boardBlockSelector = document.querySelectorAll('.block');
 
-    const Players = (name)=>{
-        return {name};
+    alert('X goes first');
+
+    //create players, also create a property to save their choice.
+    const Player = (name)=>{
+        name;
+        let choiceIndex;
+        let choiceMark;
+        return {name, choiceIndex, choiceMark};
     }
-    
-    const display = (gameBoard)=>{
-        const boardBlocks = document.querySelectorAll('.block');
-        gameBoard.forEach((value,index,obj) => {
-            boardBlocks[index].textContent = value;
+
+    const playerOne = Player('X');
+    const playerTwo = Player('O');
+    playerOne.choiceMark = 'X';
+    playerTwo.choiceMark = 'O';
+    console.log(playerOne);
+    console.log(playerTwo);
+
+    //get the index of playerChoice on the board.
+    playerOne.choiceIndex = prompt('At what index to place your mark? (0-8)', '1');
+    alert('Player O\'s turn');
+    playerTwo.choiceIndex = prompt('At what index to place your mark? (0-8)', '1');
+
+    //create an array object to save player choices.
+    const gameBoard = ((playerOne, playerTwo)=>{
+        const board = Array(9).fill('');
+
+        console.log(board);
+        console.log('PlayerOne choiceIndex: ' + playerOne.choiceIndex + ' playerTwo choiceIndex: ' + playerTwo.choiceIndex);
+
+        board[playerOne.choiceIndex] = playerOne.choiceMark;
+        board[playerTwo.choiceIndex] = playerTwo.choiceMark;
+
+        return {board};
+    })(playerOne, playerTwo);
+
+    const board = gameBoard.board;
+
+    console.log(board);
+
+
+    //create an object to display the choices.
+
+    const display = ((board)=>{
+        
+        let xIndices = [];
+        let oIndices = [];
+
+        board.forEach((currentValue, currentIndex)=>{
+            
+            if(currentValue === 'X'){
+                xIndices.push(currentIndex);
+            }
+            else if(currentValue === 'O'){
+                oIndices.push(currentIndex);
+            }
         });
-    }
 
-    return {gameBoard, Players, display};
+        console.log(xIndices);
+        console.log(oIndices);
+
+        boardBlockSelector.forEach((currentValue, currentIndex)=>{
+            xIndices.forEach((value, index)=>{
+                if(value === currentIndex){
+                    currentValue.textContent = 'X';
+                }
+            })
+        })
+
+        boardBlockSelector.forEach((currentValue, currentIndex)=>{
+            oIndices.forEach((value, index)=>{
+                if(value === currentIndex){
+                    currentValue.textContent = 'O';
+                }
+            })
+        })
+
+    })(board);
 
 })();
-
-const gameBoard = game.gameBoard();
-
-const playerOne = game.Players('X');
-const plyaerTwo = game.Players('O');
-
-const display = game.display(gameBoard);
