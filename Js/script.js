3362ab35874911c730a6ff7fb38fd72b7b28a581
headlessNode <harriskhan047@outlook.com>
2023-09-29 18:31:16 +0500

    Add initial logic of win condition

--- Js/script.js
+++ Js/script.js
@@ -31,8 +31,8 @@ const game = (()=>{
 
     const display = (()=>{
 
-        let xIndices = [];
-        let oIndices = [];
+        let xIndices = Array(9).fill(null);
+        let oIndices = Array(9).fill(null);
         let alreadyCheckedIndices = [];
 
         const updateScreen = (board)=>{
@@ -69,8 +69,24 @@ const game = (()=>{
         }
 
         const checkWin = (board)=>{
-            console.log('xIndices ' + xIndices);
-            console.log('oIndices ' + oIndices);
+            console.table(board);
+
+            if(board[0] === 'X' && board[1] ==='X' && board[2] === 'X' || board[3] === 'X' && board[4] === 'X' && board[5] === 'X'
+                || board[6] === 'X' && board[7] === 'X' && board[8] === 'X'
+                || board[0] === 'X' && board[4] === 'X' && board[8] === 'X'
+                || board[2] === 'X' && board[4] === 'X' && board[6] === 'X'){
+                
+                    console.log('X Wins the game');
+            }
+
+            if(board[0] === 'O' && board[1] ==='O' && board[2] === 'O' || board[3] === 'O' && board[4] === 'O' && board[5] === 'O'
+                || board[6] === 'O' && board[7] === 'O' && board[8] === 'O'
+                || board[0] === 'O' && board[4] === 'O' && board[8] === 'O'
+                || board[2] === 'O' && board[4] === 'O' && board[6] === 'O'){
+                
+                    console.log('O Wins the game');
+            }    
+
         }
 
         return {updateScreen, checkWin};
